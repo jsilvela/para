0200ab23ade72eb1d1ef523cb27da836245c2de6
Jaime Silvela <jaime.silvela@gmail.com>
2014-11-01 15:08:07 +0100

    Go-fmt compliance

--- para/para.go
+++ para/para.go
@@ -3,8 +3,8 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"os"
 	"log"
+	"os"
 	"unicode"
 )
 
@@ -13,10 +13,10 @@ const wrap_col = 10
 func main() {
 	var out []byte
 	s := bufio.NewReader(os.Stdin)
-	for line, err := s.ReadBytes('\n'); line!=nil; line, err = s.ReadBytes('\n') {
+	for line, err := s.ReadBytes('\n'); line != nil; line, err = s.ReadBytes('\n') {
 		j, last_white, line_chars := 0, 0, 0
 		out = make([]byte, 2*len(line))
-		for _, c := range(line) {
+		for _, c := range line {
 			out[j] = c
 			if unicode.IsSpace(rune(c)) {
 				last_white = j
