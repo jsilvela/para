0616f836a9abbeb5de2b1b871c89c4a4ad9a665a
Jaime Silvela <jaime.silvela@gmail.com>
2015-07-07 00:47:51 +0100

    Rewrite wrapper functions as methods. Add tests.

--- para/para.go
+++ para/para.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
@@ -25,35 +26,62 @@ func main() {
 	}
 	scanner := bufio.NewScanner(os.Stdin)
 	writer := bufio.NewWriter(os.Stdout)
-	err := wraptext(wrap, scanner, writer)
+	r := Rapper{maxcols: wrap}
+	err := r.wraptext(scanner, writer)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+type Rapper struct {
+	maxcols int
+	carry int
+	pending_break bool
+}
+
 // wrap text to column length, compact paragraph along the way
 // respect lines that end in a period
-func wraptext(column int, scanner *bufio.Scanner, writer *bufio.Writer) error {
+func (r Rapper) wraptext(scanner *bufio.Scanner, writer *bufio.Writer) error {
 	for scanner.Scan() {
 		line := scanner.Text()
-		writer.WriteString(wrapcol(column, line) + "\n")
+		wrapped := r.wrapline(line)
+		if r.pending_break {
+			writer.WriteString("\n")
+			r.pending_break = false
+		}
+		writer.WriteString(wrapped)
+		if strings.HasSuffix(line, ".") || len(line) == 0 {
+			// Respect paragraphs and full stops
+			r.pending_break = true
+			r.carry = 0
+		} else {
+			lastbrk := strings.LastIndex(wrapped, "\n")
+			r.carry = len(line) - 1 - lastbrk
+			if r.carry > 0 {
+				r.carry = r.carry + 1
+			}
+		}
 	}
 	return writer.Flush()
 }
 
 // wrap a single line to a colum length, possibly breaking it
-func wrapcol(colnum int, line string) string {
-	last_white, last_newline := 0, 0
+func (r Rapper) wrapline(line string) string {
+	last_white, last_newline := -1, r.carry - 1
 	out := make([]rune, 2*len(line)) // at most 1 LF per old char
 	for j, c := range line {
 		out[j] = c
 		if unicode.IsSpace(rune(c)) {
 			last_white = j
 		}
-		if j-last_newline > colnum {
+		if j - last_newline > r.maxcols && last_white > -1 {
 			out[last_white] = '\n'
 			last_newline = last_white
 		}
 	}
-	return string(out)
+	if r.carry > 0 {
+		return " " + string(out)
+	} else {
+		return string(out)
+	}
 }
--- para/para_test.go
+++ para/para_test.go
@@ -7,35 +7,86 @@ import (
 	"testing"
 )
 
-func Test_wrapcol(t *testing.T) {
-	wrapped := wrapcol(10, "12 12 12 233 23 3 345 34")
-	lines := strings.Split(wrapped, "\n")
-	if len(lines) != 3 {
-		t.Errorf("Missing newlines in: %s", wrapped)
-	}
-}
-
-func wrap_wraptext(col int, in string) string {
+func wraptext(col int, in string) string {
 	reader := strings.NewReader(in)
 	scanner := bufio.NewScanner(reader)
 	var buffer bytes.Buffer
 	writer := bufio.NewWriter(&buffer)
-	wraptext(col, scanner, writer)
+	Rapper{maxcols: col}.wraptext(scanner, writer)
 	return buffer.String()
 }
 
-func Test_wraptext_adds_newline_on_flush(t *testing.T) {
+func Test_wrap_single_line(t *testing.T) {
+	test_s := "12 12 12 233 23 3 345 34"
+	result := wraptext(10, test_s)
+	lines := strings.Split(result, "\n")
+	if len(test_s) != len(result) {
+		t.Errorf("Content was altered %d, %d:\n%s\n%s", len(test_s), 
+			len(result), test_s, result)
+	}
+	if len(lines) != 3 {
+		t.Errorf("Missing newlines in: %s", result)
+	}
+}
+
+func Test_wrap_lines(t *testing.T) {
 	test_s := "My name is Wile E. Coyote"
-	result := wrap_wraptext(15, test_s)
-	if len(strings.Split(result, "\n")) != 3 {
+	result := wraptext(15, test_s)
+	ressy := strings.Map(func (x rune) rune {
+								if x == '\n' {
+									return ' '
+								} else {
+									return x
+								} }, result)
+	if test_s !=  ressy {
+		t.Errorf("Content was altered %d, %d:\n%s\n%s", len(test_s), 
+			len(result), test_s, ressy)
+	}
+	if len(strings.Split(result, "\n")) != 2 {
 		t.Errorf("Bad wrap:\n%s", result)
 	}
 }
 
-func Test_wraptext_respects_paragraphs(t *testing.T) {
-	test_s := "My name is Wile E.\nCoyote"
-	result := wrap_wraptext(15, test_s)
+func Test_wrap_respects_one_blank_line(t *testing.T) {
+	test_s := "My name is Wile E.\nCoyote.\n\nGenius"
+	result := wraptext(15, test_s)
+	if len(test_s) != len(result) {
+		t.Errorf("Content was altered:\n%s\n%s", test_s, result)
+	}
+	if len(strings.Split(result, "\n")) != 5 {
+		t.Errorf("Didn't respect blank line:\n%s", result)
+	}
+}
+
+func Test_wrap_respects_two_blank_lines(t *testing.T) {
+	test_s := "My name is E.\n\n\nCoyote"
+	result := wraptext(15, test_s)
+	if len(test_s) != len(result) {
+		t.Errorf("Content was altered:\n%s\n%s", test_s, result)
+	}
 	if len(strings.Split(result, "\n")) != 4 {
+		t.Errorf("Didn't respect two blank lines:\n%s", result)
+	}
+}
+
+func Test_wrap_respects_full_stops(t *testing.T) {
+	test_s := "My name is Wile E.\nCoyote"
+	result := wraptext(15, test_s)
+	if len(test_s) != len(result) {
+		t.Errorf("Content was altered:\n%s\n%s", test_s, result)
+	}
+	if len(strings.Split(result, "\n")) != 3 {
+		t.Errorf("Didn't respect full stop:\n%s", result)
+	}
+}
+
+func Test_wrap_does_not_add_extra_breaks(t *testing.T) {
+	test_s := "My name is."
+	result := wraptext(15, test_s)
+	if len(test_s) != len(result) {
+		t.Errorf("Content was altered:\n%s\n%s", test_s, result)
+	}
+	if len(strings.Split(result, "\n")) != 1 {
 		t.Errorf("Didn't respect period-break:\n%s", result)
 	}
 }
